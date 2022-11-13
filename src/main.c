e4a1df063a3307768fd9f7688650aba7867ffaf0
Slav XpXz <76703277+Slav-XpXz@users.noreply.github.com>
2022-11-13 00:53:54 -0500

    added code

---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	for(int i = 0; i<100; i++){if(i%3 == 0){printf("fizz\n");}else if(i%5 == 0){printf("buzz\n");}else{printf("%d\n", i);}}
+}

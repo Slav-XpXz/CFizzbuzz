ceac0de4dcd3b6b59aea8eca7e8d8d6f3314c8ed
Slav XpXz <76703277+Slav-XpXz@users.noreply.github.com>
2022-11-13 12:54:39 -0500

    forgot the fizzbuzz part

--- src/main.c
+++ src/main.c
@@ -3,5 +3,5 @@
 
 int main(void)
 {
-	for(int i = 0; i<100; i++){if(i%3 == 0){printf("fizz\n");}else if(i%5 == 0){printf("buzz\n");}else{printf("%d\n", i);}}
+	for(int i = 0; i<100; i++){if(i%15 == 0){printf("Fizzbuzz\n");}else if(i%3 == 0){printf("fizz\n");}else if(i%5 == 0){printf("buzz\n");}else{printf("%d\n", i);}}
 }
